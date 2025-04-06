6959bc6ec5971f481f27e243e9df4c3ed10b0584
Anas Kaarov <anasbinkurban@gmail.com>
2025-04-06 17:40:24 +0600

    [4] Add JWT

--- src/users/serializers.py
+++ src/users/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 
 from .models import User
@@ -7,3 +8,61 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ("id", "username", "email", "bio", "language")
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = (
+            "username",
+            "email",
+            "language",
+            "password",
+            "password2",
+        )
+        extra_kwargs = {"username": {"required": True}, "email": {"required": True}}
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["password2"]:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        return attrs
+
+    def create(self, validated_data):
+        language = validated_data.pop("language", None)
+        user = User.objects.create(
+            username=validated_data["username"], email=validated_data["email"], language=language
+        )
+        user.set_password(validated_data["password"])
+        user.save()
+        return user
+
+
+class ChangePasswordSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+    old_password = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ("old_password", "password", "password2")
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["password2"]:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+
+        return attrs
+
+    def validate_old_password(self, attrs):
+        user = self.context["user"]
+        if not user.check_password(attrs):
+            raise serializers.ValidationError({"old_password": "Old password is not correct"})
+        return attrs
+
+    def update_password(self, validated_data):
+        user = self.context["user"]
+        new_pass = validated_data["password"]
+        user.set_password(new_pass)
+        user.save()
--- src/users/views.py
+++ src/users/views.py
@@ -1,7 +1,11 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from .models import User
-from .serializers import UserSerializer
+from .serializers import ChangePasswordSerializer, RegisterSerializer, UserSerializer
 
 
 class UserListAPIView(generics.ListAPIView):
@@ -10,3 +14,41 @@ class UserListAPIView(generics.ListAPIView):
 
     def get_queryset(self):
         return super().get_queryset().filter(username=self.request.user.username)
+
+
+class UserUpdateDestroyAPIView(generics.UpdateAPIView, generics.DestroyAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    def get_queryset(self):
+        return super().get_queryset().filter(username=self.request.user.username)
+
+
+class RegisterUserAPIView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = (AllowAny,)
+
+    def post(self, request, *args, **kwargs):
+        serializer = RegisterSerializer(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.save()
+
+            refresh = RefreshToken.for_user(user)
+            tokens = {
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+            }
+
+            return Response(tokens, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ChangePasswordView(APIView):
+    serializer_class = ChangePasswordSerializer
+
+    def post(self, request):
+        serializer = ChangePasswordSerializer(data=request.data, context={"user": request.user})
+        serializer.is_valid(raise_exception=True)
+        serializer.update_password(serializer.validated_data)
+        return Response({"msg": "Password is updated"}, status=status.HTTP_200_OK)
