4f641e58f93ae3da497f391730da105f572f0d32
Kaarov <99861500+Kaarov@users.noreply.github.com>
2025-04-20 22:01:59 +0600

    [13] Add Bar Chart (#13)

--- src/analytics/urls.py
+++ src/analytics/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
+from analytics.bar_chart.views import BarChartDataAPIView
 from analytics.pie_chart.views import PieChartDataAPIView
 
 urlpatterns = [
     path("pie/", PieChartDataAPIView.as_view(), name="pie-chart"),
+    path("bar/", BarChartDataAPIView.as_view(), name="bar-chart"),
 ]
